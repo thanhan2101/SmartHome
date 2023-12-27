9a9a5b650c23860b915babc98e9fa31006331fe2
ttan <ttan@humaxdigital.com>
2023-12-27 16:56:47 +0700

    init commit

---
+++ app/src/main/java/com/tommyhumaxcar/smarthome/LoginPage.java
@@ -0,0 +1,31 @@
+package com.tommyhumaxcar.smarthome;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+
+        Button loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showToast("Login clicked");
+            }
+        });
+
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+}
