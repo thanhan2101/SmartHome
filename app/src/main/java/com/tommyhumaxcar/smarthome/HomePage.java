a5c044c25894fa271e344d5aea016ccc589acbd8
ttan <ttan@humaxdigital.com>
2023-12-28 18:57:16 +0700

    add fragment to HomePage

--- app/src/main/java/com/tommyhumaxcar/smarthome/HomePage.java
+++ app/src/main/java/com/tommyhumaxcar/smarthome/HomePage.java
@@ -1,14 +1,37 @@
 package com.tommyhumaxcar.smarthome;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
 
 public class HomePage extends AppCompatActivity {
-
+    private final String TAG = "HomePage";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        //add toolbar as actionbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TemperatureFragment temperatureFragment = TemperatureFragment.getInstance();
+
+        //add fragment
+        loadFragment(temperatureFragment, "", "");
+    }
+
+    private void loadFragment(Fragment fragment, String tag, String backStackName) {
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.frameFragment, fragment , tag)
+                .addToBackStack(backStackName)
+                .commit();
     }
 }
--- app/src/main/java/com/tommyhumaxcar/smarthome/LoginPage.java
+++ app/src/main/java/com/tommyhumaxcar/smarthome/LoginPage.java
@@ -1,6 +1,8 @@
 package com.tommyhumaxcar.smarthome;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,6 +21,7 @@ public class LoginPage extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 showToast("Login clicked");
+                loginHomePage();
             }
         });
 
@@ -26,6 +29,11 @@ public class LoginPage extends AppCompatActivity {
 
     private void showToast(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void loginHomePage() {
+        Intent loginHomePageIntent = new Intent(this, HomePage.class);
+        startActivity(loginHomePageIntent);
     }
 
 }
