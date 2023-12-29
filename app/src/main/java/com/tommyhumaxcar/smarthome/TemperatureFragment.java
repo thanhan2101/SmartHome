b5eafde2dc0945432a819e1e7af17255b6292984
ttan <ttan@humaxdigital.com>
2023-12-29 11:02:00 +0700

    set position and background of tem_frag

--- app/src/main/java/com/tommyhumaxcar/smarthome/TemperatureFragment.java
+++ app/src/main/java/com/tommyhumaxcar/smarthome/TemperatureFragment.java
@@ -8,11 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TemperatureFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class TemperatureFragment extends Fragment {
 
 //    // TO DO: Rename parameter arguments, choose names that match
